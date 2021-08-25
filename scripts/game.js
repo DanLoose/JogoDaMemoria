f466be5ad1649e7a5fbe3e53522b6bd7a0f786d4
Danilo Loose <danloose14@gmail.com>
2021-08-25 20:42:31 -0300

    card implementado e estilizado

---
+++ scripts/game.js
@@ -0,0 +1,13 @@
+const FRONT = "front-card";
+const BACK = "back-card";
+
+let techs = ['bootstrap',
+    'css',
+    'electron',
+    'firebase',
+    'html',
+    'javascript',
+    'jquery',
+    'mongo',
+    'node',
+    'react'];
